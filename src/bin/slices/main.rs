b71a18f4702a3cd19e32b7b5958f8aaf4965a431
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-09-26 14:26:00 +0800

    Added slices example

---
+++ src/bin/slices/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    println!("{word}");
+
+    let word = first_word(&my_string[..]);
+    println!("{word}");
+
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+    println!("{word}");
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    println!("{word}");
+    let word = first_word(&my_string_literal[..]);
+    println!("{word}");
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+    println!("{word}");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
