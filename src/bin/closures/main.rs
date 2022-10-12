4cbf9d0622e93a071c57c67753cdd871099387c8
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-10-12 21:43:43 +0800

    Added closures example

---
+++ src/bin/closures/main.rs
@@ -0,0 +1,121 @@
+use std::thread;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_pref: Option<ShirtColor>) -> ShirtColor {
+        user_pref.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Blue => num_blue += 1,
+                ShirtColor::Red => num_red += 1,
+            }
+        }
+
+        if num_blue > num_red {
+            ShirtColor::Blue
+        } else {
+            ShirtColor::Red
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+
+    // capturing refs and moving ownership
+    println!("immutable borrow:");
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    println!("mutable borrow:");
+
+    let mut list2 = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list2);
+
+    let mut borrows_mutably = || {
+        list2.push(7);
+    };
+
+    // immutable borrow is not allowed because the
+    // list2 has been captured as mutable borrow
+    // println!("Before calling closure: {:?}", list2);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list2);
+
+    // move ownership with 'move' keyword
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    // move is required here because the new thread
+    // might outlive the main thread, and thus will make
+    // the list an invalid reference
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
+
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
+}
