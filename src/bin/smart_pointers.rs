4695ca3da8d30199a07635b25e10542ae1d7c14b
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-10-18 20:59:45 +0800

    Added rc (reference counting) example

--- src/bin/smart_pointers.rs
+++ src/bin/smart_pointers.rs
@@ -1,7 +1,8 @@
 use std::mem::drop;
 use std::ops::Deref;
+use std::rc::Rc;
 
-use rust_examples::list::List::{Cons, Nil};
+use rust_examples::list::{List, ListV2};
 
 struct MyBox<T>(T);
 
@@ -34,7 +35,10 @@ impl Drop for CustomSmartPointer {
 }
 
 fn main() {
-    let l = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let l = List::Cons(
+        1,
+        Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
+    );
 
     let x = 5;
     let y = MyBox::new(x);
@@ -59,4 +63,17 @@ fn main() {
     println!("CustomSmartPointer created.");
     drop(d);
     println!("CustomSmartPointer dropped before the end of main.");
+
+    let a = Rc::new(ListV2::Cons(
+        5,
+        Rc::new(ListV2::Cons(10, Rc::new(ListV2::Nil))),
+    ));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = ListV2::Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = ListV2::Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a))
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
--- src/list.rs
+++ src/list.rs
@@ -1,4 +1,11 @@
+use std::rc::Rc;
+
 pub enum List<T> {
     Cons(T, Box<List<T>>),
+    Nil,
+}
+
+pub enum ListV2<T> {
+    Cons(T, Rc<ListV2<T>>),
     Nil,
 }
