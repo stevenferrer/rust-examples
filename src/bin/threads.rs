d82716eb4d3cd1b48bc92210967f04786b6414f4
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-10-30 15:29:53 +0800

    Added threads example

---
+++ src/bin/threads.rs
@@ -0,0 +1,30 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {} from the spawned thread!", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+
+    // // main thread waits for spawned thread before proceeding
+    // // handle.join().unwrap();
+
+    // for i in 1..5 {
+    //     println!("hi number {} from the main thread!", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    // // main and spawned thread interleaves
+    // // handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
