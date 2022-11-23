31bf1c764ca773e95ded40eb21abb60674c8a98d
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-11-23 18:08:46 +0800

    Added more patterns example

--- src/bin/patterns.rs
+++ src/bin/patterns.rs
@@ -1,4 +1,10 @@
 fn main() {
+    basic_patterns();
+
+    more_patterns();
+}
+
+fn basic_patterns() {
     // match arms
     let v = Some(1);
     let x: i32 = match v {
@@ -41,4 +47,214 @@ fn main() {
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
     println!("Current location: ({}, {})", x, y);
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+    ChangeColorV2(Color),
+}
+
+enum Msg {
+    Hello { id: i32 },
+}
+
+fn more_patterns() {
+    // Matching literals
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // Matching named variables
+    let x = Some(5);
+    let y = 10;
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    // Multiple guards
+    let x = 1;
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // Matching ranges of values with ..=
+    let x = 5;
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    // Destructuring structs
+    let p = Point { x: 0, y: 7 };
+
+    // long version
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    // shorthand version
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    // with match expression
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    // Destructuring enums
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+        _ => {}
+    }
+
+    // Destructuring nested structs and enums
+    let msg = Message::ChangeColorV2(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColorV2(Color::Rgb(r, g, b)) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+        Message::ChangeColorV2(Color::Hsv(h, s, v)) => println!(
+            "Change the color to hue {}, saturation {}, and value {}",
+            h, s, v
+        ),
+        _ => (),
+    }
+
+    // Destructuring structs and tuples
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    // Ignoring an entire value with _
+    foo(3, 4);
+
+    // Ignoring parts of a value with nested _
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}")
+        }
+    }
+
+    // Ignoring unsued variable
+    let _x = 5;
+
+    // Ignoring remaining parts of the value with ..
+    let origin = Point { x: 0, y: 0 };
+
+    match origin {
+        Point { x, .. } => println!("x is {}", x),
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+
+    // Match guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
+
+    // Using outer variables via match guard
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+
+    // Using or operator with match guard
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+
+    // @ bindings
+
+    let msg = Msg::Hello { id: 5 };
+
+    match msg {
+        Msg::Hello { id: id @ 3..=7 } => println!("Found an id in range: {}", id),
+        Msg::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Msg::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
 }
